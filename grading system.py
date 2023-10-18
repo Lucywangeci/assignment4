e3754e69a668de8e1b0c9a8f008c2679deb4a4c3
unknown <wangecilucy1234@gmail.com>
2023-10-18 07:19:26 +0300

    first commit

---
+++ grading system.py
@@ -0,0 +1,28 @@
+def calculate_grade(maths, physics, geo, chem):
+
+  if any(x < 0 or x > 100 for x in [maths, physics, geo, chem]):
+    return "Unrecognized marks/avg"
+
+
+  average = (maths + physics + geo + chem) / 4
+
+
+  if 0 <= average <= 30:
+    return "D"
+  elif 31 <= average <= 50:
+    return "C"
+  elif 51 <= average <= 70:
+    return "B"
+  elif 71 <= average <= 100:
+    return "A"
+
+
+
+maths = float(input("Enter Maths marks: "))
+physics = float(input("Enter Physics marks: "))
+geo = float(input("Enter Geography marks: "))
+chem = float(input("Enter Chemistry marks: "))
+
+# Calculate and display the grade
+grade = calculate_grade(maths, physics, geo, chem)
+print(f"Average Grade: {grade}")
